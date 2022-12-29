964b549de269cefa6a51577c77ce84e02cfa6f4c
2115-27 <103985748+2115-27@users.noreply.github.com>
2022-12-29 12:45:59 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -3,8 +3,6 @@ import React, { useState } from 'react';
 function Login(props) {
   const username = useFormInput('');
   const password = useFormInput('');
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
  
   // handle button click of login form
   const handleLogin = () => {
